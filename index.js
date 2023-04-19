a399033984e1390bce38efc0d52c44c74f13d62b
Your Name <you@example.com>
2023-04-19 14:02:17 +0530

    second commit

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const express=require("express");
-const app=express();
-var numberOfDrums=7;
-for(var i=0;i<7;i++)
-{
-  app.querySelectorAll(".drum")[i].addEventListener("click",function (){
+const express = require("express");
+const app = express();
+var numberOfDrums = 7;
+for (var i = 0; i < 7; i++) {
+  app.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHtml = this.innerHTML;
-  makeSound(buttonInnerHtml);
+    makeSound(buttonInnerHtml);
   });
 }
-ap.addEventListener("keypress",function(event){
+ap.addEventListener("keypress", function (event) {
   makeSound(event.key);
-})
-function makeSound(key){
+});
+
+function makeSound(key) {
   switch (key) {
     case "w":
-    var tom1=new Audio("sounds/tom-1.mp3");
-    tom1.play();
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
       break;
-      case "a":
-      var tom2=new Audio("sounds/tom-2.mp3");
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
-      case 's':
-      var tom3=new Audio("sounds/tom-3.mp3");
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
-      case 'd':
-      var tom4=new Audio("sounds/tom-4.mp3");
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
-      case 'k':
-      var crash=new Audio("sounds/crash.mp3");
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
-      case 'l':
-      var kickbass=new Audio("sounds/kick-bass.mp3");
+    case "l":
+      var kickbass = new Audio("sounds/kick-bass.mp3");
       kickbass.play();
       break;
-      case 'j':
-      var snare=new Audio("sounds/snare.mp3");
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
+  }
 }
-}
-app.listen(process.env.PORT || 4000,function(){
+app.listen(process.env.PORT || 4000, function () {
   console.log("Port running!");
 });
